4d11ebc91e2d461158a4c4696b0a9822333c3880
worspaces <vailw@rrms.wlwv.k12.or.us>
2024-11-16 14:34:34 -0800

    Update DOORS-FLOOR-3--by BDEVS-Inf Rebirths.lua

--- DOORS-FLOOR-3--by BDEVS-Inf Rebirths.lua
+++ DOORS-FLOOR-3--by BDEVS-Inf Rebirths.lua
@@ -1,1 +1,8 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "script executed", Text = "enjoy lol", Icon = "rbxassetid://139470246242494", Duration = 5})
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "orbs on scriptblox", Text = "https://scriptblox.com/u/Orbs", Icon = "rbxassetid://139470246242494", Duration = 5})
+
+game:GetService("ReplicatedStorage").Elevator:FireServer("CreateDoor", false, 69420)
+
+while wait() do
 game:GetService("MarketplaceService"):SignalPromptProductPurchaseFinished(game:GetService("Players").LocalPlayer.UserId, 2656436494, true)
+end
